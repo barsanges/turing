7281df98e458c54329602163a2d68f266e661bb2
barsanges <barsanges@users.noreply.github.com>
2021-08-12 08:11:27 +0200

    Use 'parse' and 'unparse' for Sudokus.

--- src/Games/Sudoku.hs
+++ src/Games/Sudoku.hs
@@ -17,11 +17,12 @@ module Games.Sudoku (
 
 import Data.Either ( lefts, rights )
 import qualified Data.Set as S
-import Data.Vector ( Vector, (!), (//), generateM )
+import Data.Vector ( Vector, (!), (//) )
 import Commons.Cell ( Hole, Cell, hFromSet, hToSet, hSize, hDifference,
                       hDifference', hNotIn, cToChar )
 import Commons.Digit ( Digit(..) )
 import Commons.Iterator ( solveWithIt )
+import Commons.Parsing ( parse, unparse )
 
 -- | A Sudoku grid.
 newtype Sudoku = Sk (Vector (Cell Digit))
@@ -40,15 +41,29 @@ data Dir = Column
          | Block
   deriving Show
 
+-- | An ASCII template for a Sudoku grid.
+template :: String
+template = "\
+\-------------\n\
+\|___|___|___|\n\
+\|___|___|___|\n\
+\|___|___|___|\n\
+\-------------\n\
+\|___|___|___|\n\
+\|___|___|___|\n\
+\|___|___|___|\n\
+\-------------\n\
+\|___|___|___|\n\
+\|___|___|___|\n\
+\|___|___|___|\n\
+\-------------\n\
+\"
+
 -- | Parse a string into a Sudoku grid.
 fromString :: String -> Maybe Sudoku
-fromString s0 = if length s1 == 81
-  then fmap Sk (generateM 81 (\ i -> fromChar (s1 !! i)))
-  else Nothing
+fromString s = fmap Sk (parse template '_' fromChar s)
 
   where
-
-    s1 = filter (\ c -> c /= '-' && c /= '|') (concat $ lines s0)
 
     fromChar :: Char -> Maybe (Cell Digit)
     fromChar '1' = Just (Left One)
@@ -60,37 +75,18 @@ fromString s0 = if length s1 == 81
     fromChar '7' = Just (Left Seven)
     fromChar '8' = Just (Left Eight)
     fromChar '9' = Just (Left Nine)
-    fromChar '_' = Just (none)
+    fromChar '_' = Just (Right (hFromSet One Two (S.fromList [Three,
+                                                              Four,
+                                                              Five,
+                                                              Six,
+                                                              Seven,
+                                                              Eight,
+                                                              Nine])))
     fromChar _ = Nothing
-
-    none = Right (hFromSet One Two (S.fromList [Three,
-                                                Four,
-                                                Five,
-                                                Six,
-                                                Seven,
-                                                Eight,
-                                                Nine]))
 
 -- | Turn a Sudoku grid into a string.
 toString :: Sudoku -> String
-toString (Sk vec) =
-  unlines
-  [ "-------------"
-  , ['|', f  0, f  1, f  2, '|', f  3, f  4, f  5, '|', f  6, f  7, f  8, '|']
-  , ['|', f  9, f 10, f 11, '|', f 12, f 13, f 14, '|', f 15, f 16, f 17, '|']
-  , ['|', f 18, f 19, f 20, '|', f 21, f 22, f 23, '|', f 24, f 25, f 26, '|']
-  ,  "-------------"
-  , ['|', f 27, f 28, f 29, '|', f 30, f 31, f 32, '|', f 33, f 34, f 35, '|']
-  , ['|', f 36, f 37, f 38, '|', f 39, f 40, f 41, '|', f 42, f 43, f 44, '|']
-  , ['|', f 45, f 46, f 47, '|', f 48, f 49, f 50, '|', f 51, f 52, f 53, '|']
-  ,  "-------------"
-  , ['|', f 54, f 55, f 56, '|', f 57, f 58, f 59, '|', f 60, f 61, f 62, '|']
-  , ['|', f 63, f 64, f 65, '|', f 66, f 67, f 68, '|', f 69, f 70, f 71, '|']
-  , ['|', f 72, f 73, f 74, '|', f 75, f 76, f 77, '|', f 78, f 79, f 80, '|']
-  , "-------------"
-  ]
-  where
-    f i = cToChar (vec ! i)
+toString (Sk vec) = unparse template '_' cToChar vec
 
 -- | Indexers for all possible views of a grid.
 allViews :: [(Int, Dir)]
